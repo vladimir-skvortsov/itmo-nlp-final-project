e173473eacbd204ba40151a427646fb089f5f2ad
Vladimir Skvortsov <mail@vladimirskvortsov.com>
2026-05-10 19:07:25 +0300

    feat: add __init__.py for evaluation metrics in relation extraction

---
+++ src/nerel_re/evaluation/__init__.py
@@ -0,0 +1,5 @@
+"""Evaluation metrics for relation extraction."""
+
+from nerel_re.evaluation.metrics import compute_metrics, per_class_report, print_results_table
+
+__all__ = ['compute_metrics', 'per_class_report', 'print_results_table']
